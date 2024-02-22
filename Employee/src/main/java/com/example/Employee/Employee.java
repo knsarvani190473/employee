4be18474d07fb82d6e93a9a86af5ea5a855d79ea
knsarvani190473 <160032201+knsarvani190473@users.noreply.github.com>
2024-02-22 20:46:27 +0530

    Add files via upload

--- Employee/src/main/java/com/example/Employee/Employee.java
+++ Employee/src/main/java/com/example/Employee/Employee.java
@@ -2,8 +2,10 @@ package com.example.Employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Employee {
 
 	public static void main(String[] args) {
--- Employee/src/main/java/com/example/Employee/service/EmployeeService.java
+++ Employee/src/main/java/com/example/Employee/service/EmployeeService.java
@@ -5,7 +5,10 @@ import com.example.Employee.entity.EmployeeDTO;
 import com.example.Employee.exception.EmployeeNotFoundException;
 import com.example.Employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
+import springfox.documentation.annotations.Cacheable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,49 +19,55 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
-public List<EmployeeDTO> getAllEmployees(){
-
-    List<Employee> employees = employeeRepository.findAll();
-    return employees.stream()
+    //indicates that result of method is cached,
+    //value "cars" is name of the cache
+    @Cacheable("employees")
+    public List<EmployeeDTO> getAllEmployees(){
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream()
             .map(this::convertToDTO)
             .collect(Collectors.toList());
-}
-public EmployeeDTO getEmployeeById(Long id){
-    Employee employee = employeeRepository.findById(id).orElseThrow(()->new EmployeeNotFoundException(id));
-    return convertToDTO(employee);
-}
-public EmployeeDTO createEmployee(EmployeeDTO newEmployeeDTO){
-    Employee employee = convertToEntity(newEmployeeDTO);
-    Employee savedEmployee = employeeRepository.save(employee);
-    return convertToDTO(savedEmployee);
-}
-public void deleteEmployeeById(Long id){
-    employeeRepository.deleteById(id);
-}
-public EmployeeDTO updateEmployee(Long id, EmployeeDTO updatedEmployeeDTO){
-    Employee existingEmployee = employeeRepository.findById(id).orElseThrow(()-> new EmployeeNotFoundException(id));
-    existingEmployee.setId(updatedEmployeeDTO.getId());
-    existingEmployee.setName(updatedEmployeeDTO.getName());
-    existingEmployee.setRole(updatedEmployeeDTO.getRole());
-    Employee updatedEmployee = employeeRepository.save(existingEmployee);
-    return convertToDTO(updatedEmployee);
-}
+    }
+    @Cacheable("employeeById")
+    public EmployeeDTO getEmployeeById(Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(()->new EmployeeNotFoundException(id));
+        return convertToDTO(employee);
+    }
+    @CachePut(value = "employeeById")
+    public EmployeeDTO createEmployee(EmployeeDTO newEmployeeDTO){
+        Employee employee = convertToEntity(newEmployeeDTO);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return convertToDTO(savedEmployee);
+    }
+    @CacheEvict(cacheNames = {"employees" , "employeeById"})
+    public void deleteEmployeeById(Long id){
 
-private EmployeeDTO convertToDTO(Employee employee){
-    EmployeeDTO employeeDTO = new EmployeeDTO();
-    employeeDTO.setId(employee.getId());
-    employeeDTO.setName(employee.getName());
-    employeeDTO.setRole(employee.getRole());
-    employeeDTO.setSkills(employee.getSkills());
-    return employeeDTO;
-}
-private Employee convertToEntity(EmployeeDTO employeeDTO){
-    Employee employee = new Employee();
-    employee.setId(employeeDTO.getId());
-    employee.setName(employeeDTO.getName());
-    employee.setRole(employeeDTO.getRole());
-    employee.setSkills(employeeDTO.getSkills());
-   return employee;
-}
+        employeeRepository.deleteById(id);
+    }
+    @CachePut(value = "employeeById")
+    public EmployeeDTO updateEmployee(Long id, EmployeeDTO updatedEmployeeDTO){
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(()-> new EmployeeNotFoundException(id));
+        existingEmployee.setId(updatedEmployeeDTO.getId());
+        existingEmployee.setName(updatedEmployeeDTO.getName());
+        existingEmployee.setRole(updatedEmployeeDTO.getRole());
+        Employee updatedEmployee = employeeRepository.save(existingEmployee);
+        return convertToDTO(updatedEmployee);
+    }
+    private EmployeeDTO convertToDTO(Employee employee){
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        employeeDTO.setId(employee.getId());
+        employeeDTO.setName(employee.getName());
+        employeeDTO.setRole(employee.getRole());
+        employeeDTO.setSkills(employee.getSkills());
+        return employeeDTO;
+    }
+    private Employee convertToEntity(EmployeeDTO employeeDTO){
+        Employee employee = new Employee();
+        employee.setId(employeeDTO.getId());
+        employee.setName(employeeDTO.getName());
+        employee.setRole(employeeDTO.getRole());
+        employee.setSkills(employeeDTO.getSkills());
+        return employee;
+    }
 
 }
